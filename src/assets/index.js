4c15742646e019ded3f0c9fed7f8e3e0e4709865
jeebeez <bhattgauravd@gmail.com>
2022-02-13 22:01:10 -0800

    added fonts and assests

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as Clover } from './Clover.svg';
+export { default as Diamond } from './Diamond.svg';
+export { default as Heart } from './Heart.svg';
+export { default as Spade } from './Spade.svg';
+export { default as winner } from './winner.svg';
+export { default as Back } from './Back.jpg';
